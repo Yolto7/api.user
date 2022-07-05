69a297db8ec63460c4f8e8d12c10f3b2afe820ba
Joaquin <joaquin.torres@unmsm.edu.pe>
2022-07-04 21:02:51 -0500

    finished api.user

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const { dbConnection } = require("./config/database");
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT
 
 //DATABASE
 dbConnection()
